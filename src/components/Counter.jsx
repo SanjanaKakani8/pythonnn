f774461405b4b913182c55b103ba825e16f17b6a
Sanjana <kakanisanjanareddy8@gmail.com>
2026-03-10 09:36:31 +0530

    commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+function Counter(){
+  const[count, setCount]= useState(0);
+  return (
+    <>
+    <div className='card'>
+      <button onClick={() => setCount((count) => count + 1)
+      }> count is {count}</button>
+      <h1> Sanjana</h1>
+    </div>
+    </>
+  );
+}
+export default Counter
